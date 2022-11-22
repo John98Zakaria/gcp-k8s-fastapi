e518e1f1d593a4e0030c4a831e721ec19738be8e
jsorial <jsorial@kpmg.com>
2022-11-22 07:50:39 +0100

    chore: Made flake8 happier.

--- src/sqlalchemymodels/DBUserModel.py
+++ src/sqlalchemymodels/DBUserModel.py
@@ -3,16 +3,24 @@ import uuid
 from typing import List
 
 from pydantic import EmailStr
-from sqlalchemy import Column, ForeignKey, func, text, select
+from sqlalchemy import Column
 from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import func
+from sqlalchemy import select
+from sqlalchemy import text
 from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy.orm import relationship, column_property
+from sqlalchemy.orm import column_property
+from sqlalchemy.orm import relationship
 from sqlalchemy_utils import UUIDType
 
-from general_types.typesafe_representations import Username, GID
+from general_types.typesafe_representations import GID
+from general_types.typesafe_representations import Username
 from sqlalchemymodels.auditing import SQLBase
-from sqlalchemymodels.predifined_types import SQLNameStr, SQLEmailStr, TweetStr
+from sqlalchemymodels.predifined_types import SQLEmailStr
+from sqlalchemymodels.predifined_types import SQLNameStr
 from sqlalchemymodels.predifined_types import SQLUsernameStr
+from sqlalchemymodels.predifined_types import TweetStr
 
 
 def register_user_model():
@@ -21,9 +29,18 @@ def register_user_model():
 
 class DBTweetModel(SQLBase):
     __tablename__ = "tweets"
-    tweet_id: GID = Column(UUIDType, default=lambda: str(uuid.uuid4()), primary_key=True)
-    creator: Username = Column(SQLUsernameStr, ForeignKey("users.username", ondelete="cascade", ), nullable=False,
-                               index=True)
+    tweet_id: GID = Column(
+        UUIDType, default=lambda: str(uuid.uuid4()), primary_key=True
+    )
+    creator: Username = Column(
+        SQLUsernameStr,
+        ForeignKey(
+            "users.username",
+            ondelete="cascade",
+        ),
+        nullable=False,
+        index=True,
+    )
 
     tweet_content: str = Column(TweetStr, nullable=False)
 
@@ -35,13 +52,17 @@ class DBUserModel(SQLBase):
     birthdate: datetime.date = Column(DateTime, nullable=False)
     first_name: str = Column(SQLNameStr, nullable=False)
     last_name: str = Column(SQLNameStr, nullable=False)
-    tweets: List["DBTweetModel"] = relationship("DBTweetModel", uselist=True, passive_deletes=True,
-                                                cascade="all, delete-orphan")
+    tweets: List["DBTweetModel"] = relationship(
+        "DBTweetModel", uselist=True, passive_deletes=True, cascade="all, delete-orphan"
+    )
 
     num_tweets = column_property(
-        select(func.count(text("tweet_id"))).where(DBTweetModel.creator == username).scalar_subquery())
+        select(func.count(text("tweet_id")))
+        .where(DBTweetModel.creator == username)
+        .scalar_subquery()
+    )
 
-    ## Hybrid_properties are evaluated lazily
+    # Hybrid_properties are evaluated lazily
     @hybrid_property
     def num_tweets_hybrid(self):
         if self.tweets:
@@ -49,8 +70,10 @@ class DBUserModel(SQLBase):
         return 0
 
     @num_tweets_hybrid.expression
-    def num_tweets_expr(cls):
-        return select(func.count(DBTweetModel.tweet_id)).where(DBTweetModel.creator == cls.username)
+    def num_tweets_expr(cls):  # noqa: B902, N805
+        return select(func.count(DBTweetModel.tweet_id)).where(
+            DBTweetModel.creator == cls.username
+        )
 
     @property
     def fullname(self):
